202827054d0009fcb5a96eb4ef852ec1732ccffc
jennysival <jeniffersival@gmail.com>
2022-04-05 11:00:16 -0300

    Criar mensagem de boas vindas

---
+++ src/DesafioSimCity.java
@@ -0,0 +1,7 @@
+public class DesafioSimCity {
+    public static void main(String[] args) {
+        System.out.println("----------------------------");
+        System.out.println("Seja bem-vinde ao sistema de\ncálculo de imposto de SimCity");
+        System.out.println("----------------------------");
+    }
+}
