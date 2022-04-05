eb27efd5ba6f46983eef0d7203a7114b3678ecd0
jennysival <jeniffersival@gmail.com>
2022-04-05 11:31:13 -0300

    Arrumar a visualização do menu

--- src/DesafioSimCity.java
+++ src/DesafioSimCity.java
@@ -5,16 +5,13 @@ public class DesafioSimCity {
         System.out.println("----------------------------");
         System.out.println("Seja bem-vinde ao sistema de\ncálculo de imposto de SimCity");
         System.out.println("----------------------------");
-        System.out.println("\nSou Simmy, a assistente virtual de Sim City\nComo posso te ajudar?");
+        System.out.println("\nSou Simmy, a assistente virtual de Sim City\nComo posso te ajudar?\n");
 
         menu();;
     }
 
     public static void menu(){
         Scanner leitor = new Scanner(System.in);
-        
-        System.out.println("\nDigite apenas o número da opção escolhida:\n");
-        int numSelecionado = leitor.nextInt();
         System.out.println("1. Cadastrar nome e profissão;");
         System.out.println("2. Visualizar dados cadastrados;");
         System.out.println("3. Cadastrar salários;");
@@ -22,14 +19,16 @@ public class DesafioSimCity {
         System.out.println("5. Calcular o imposto;");
         System.out.println("6. Sair;");
 
+        System.out.println("\nDigite apenas o número da opção escolhida:");
+        int numSelecionado = leitor.nextInt();
+        
+
         if(numSelecionado == 1){
             cadNomeProfissa();
         }
-
         else if(numSelecionado == 6){
             System.exit(0);
         }
-
         else{
             System.out.println("Número digitado não corresponde aos disponíveis no menu, por favor comece novamente.");
         }
